ede00b13adedbd9c4613a4e407318cfde6ca84e5
Adam Komisarek <akakus@gmail.com>
2022-03-11 20:28:35 +0000

    Fixed the interface for lottery config

--- src/main/java/com/mevdev/lotterymachine/lottery/LotteryConfig.java
+++ src/main/java/com/mevdev/lotterymachine/lottery/LotteryConfig.java
@@ -1,22 +1,26 @@
 package com.mevdev.lotterymachine.lottery;
 
 public class LotteryConfig {
-    private final int giftedTokens;
+    private final int giftedTier1Tokens;
+    private final int giftedTier2Tokens;
+    private final int giftedTier3Tokens;
     private final int tier1Tokens;
     private final int tier2Tokens;
     private final int tier3Tokens;
     private final int primeTokens;
 
-    public LotteryConfig(int giftedTokens, int tier1Tokens, int tier2Tokens, int tier3Tokens, int primeTokens) {
-        this.giftedTokens = giftedTokens;
+    public LotteryConfig(int giftedTier1Tokens, int giftedTier2Tokens, int giftedTier3Tokens, int tier1Tokens, int tier2Tokens, int tier3Tokens, int primeTokens) {
+        this.giftedTier1Tokens = giftedTier1Tokens;
+        this.giftedTier2Tokens = giftedTier2Tokens;
+        this.giftedTier3Tokens = giftedTier3Tokens;
         this.tier1Tokens = tier1Tokens;
         this.tier2Tokens = tier2Tokens;
         this.tier3Tokens = tier3Tokens;
         this.primeTokens = primeTokens;
     }
 
-    public int getGiftedTokens() {
-        return giftedTokens;
+    public int getGiftedTier1Tokens() {
+        return giftedTier1Tokens;
     }
 
     public int getTier1Tokens() {
--- src/test/main/java/com/mevdev/lotterymachine/lottery/LotteryMachineTest.java
+++ src/test/main/java/com/mevdev/lotterymachine/lottery/LotteryMachineTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LotteryMachineTest {
@@ -15,18 +16,31 @@ class LotteryMachineTest {
     @Test
     public void shouldRejectNullSubscribers() {
         assertThrows(IllegalArgumentException.class, () -> new LotteryMachine(null,
-                new LotteryConfig(0, 0, 0, 0, 0)));
+                getLotteryConfig()));
+    }
+
+    private LotteryConfig getLotteryConfig() {
+        return new LotteryConfig(0,0,0,0, 0, 0, 0);
     }
 
     @Test
     public void shouldRejectEmptySubscribers() {
         assertThrows(IllegalArgumentException.class, () -> new LotteryMachine(new ArrayList<>(),
-                new LotteryConfig(0, 0, 0, 0, 0)));
+                getLotteryConfig()));
     }
 
     @Test
     public void shouldRejectNullConfig() {
         List<Subscriber> subscribers = List.of(new Subscriber("Adam", Tier.FIRST, SubType.NORMAL));
         assertThrows(NullPointerException.class, () -> new LotteryMachine(subscribers, null));
+    }
+
+    @Test
+    public void shouldSelectSubscriberWhenOnlyOneIsAvailable() {
+        List<Subscriber> subscribers = List.of(new Subscriber("Adam", Tier.FIRST, SubType.NORMAL));
+        LotteryMachine lotteryMachine = new LotteryMachine(subscribers, null);
+
+        Subscriber subscriber = lotteryMachine.selectSubscriber();
+        assertEquals(subscribers.get(0), subscriber);
     }
 }
